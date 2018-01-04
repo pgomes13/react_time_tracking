dec0ed03194a1b62b4af4569e3eae579b8a91f75
Pascoal Gomes <pascoal@mfdc.biz>
2018-01-04 19:35:30 +1000

    added TimerDashboard component

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
-
+class TimerDashboard extends React.Component {
+	render() {
+		return (
+			<div className='ui three column centered grid'>
+				<div className='column'>
+					<EditableTimerList />
+					<ToggleableTimerForm
+						isOpen={true}
+					/>
+				</div>
+			</div>	
+		);
+	}
+}
