180bc3c42e185725c2beb46444ce4d01457b1141
Pascoal Gomes <Pascoal.Gomes@comparethemarket.com.au>
2018-01-12 16:35:37 +1000

    added functionality to add new timers

--- public/js/app.js
+++ public/js/app.js
@@ -18,6 +18,17 @@ class TimersDashboard extends React.Component {
 		]
 	};
 
+	handleCreateFormSubmit = (timer) => {
+		this.createTimer(timer);
+	}
+
+	createTimer =(timer) => {
+		const t = helpers.newTimer(timer);
+		this.setState({
+			timers: this.state.timers.concat(t)
+		});
+	}
+
 	render() {
 		return (
 			<div className='ui three column centered grid'>
@@ -25,7 +36,9 @@ class TimersDashboard extends React.Component {
 					<EditableTimerList 
 						timers={this.state.timers}
 					/>
-					<ToggleableTimerForm />
+					<ToggleableTimerForm 
+						onFormSubmit={this.handleCreateFormSubmit}
+					/>
 				</div>
 			</div>	
 		);
@@ -41,10 +54,22 @@ class ToggleableTimerForm extends React.Component {
 		this.setState({ isOpen: true });
 	}
 
+	handleFormClose = () => {
+		this.setState({ isOpen: false });
+	}
+
+	handleFormSubmit = (timer) => {
+		this.props.onFormSubmit(timer);
+		this.setState({ isOpen: false });
+	}
+
 	render() {
 		if (this.state.isOpen) {
 			return (
-				<TimerForm />
+				<TimerForm 
+					onFormSubmit={this.handleFormSubmit}
+					onFormClose={this.handleFormClose}
+				/>
 			);
 		} else {
 			return (
@@ -157,8 +182,16 @@ class TimerForm extends React.Component {
 		this.setState({ project: e.target.value });
 	}
 
+	handleSubmit = () => {
+		this.props.onFormSubmit({
+			id: this.props.id,
+			title: this.state.title,
+			project: this.state.project
+		});
+	}
+
 	render() {
-		const submitText = this.props.title ? 'Update' : 'Create';
+		const submitText = this.props.id ? 'Update' : 'Create';
 		return (
 			<div className='ui centered card'>
 	        	<div className='content'>
@@ -180,10 +213,14 @@ class TimerForm extends React.Component {
 							/>
 	            		</div>
 	            		<div className='ui two bottom attached buttons'>
-	              			<button className='ui basic blue button'>
+	              			<button 
+	              				className='ui basic blue button'
+	              				onClick={this.handleSubmit}>
 	                			{submitText}
 	              			</button>
-	              			<button className='ui basic red button'>
+	              			<button 
+	              				className='ui basic red button'
+	              				onClick={this.props.onFormClose}>
 								Cancel
 	              			</button>
 	            		</div>
